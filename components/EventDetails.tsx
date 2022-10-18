db351e88aa19e6de369f9bd61e432ee83da79feb
Silvère Lestang <silvere.lestang@elements-apps.com>
2022-10-18 22:54:45 +0200

    Revert "Move most icons to Lucide"
    This reverts commit 151824a4b62e3db942c557fc80cfcdca98e87726.

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -1,44 +1,24 @@
 import Link from "next/link";
 
-import { createIcons, Send, Mic2, Code, LifeBuoy, HeartHandshake, Baby, Beer, Coins, Gamepad2, Truck, Shirt } from 'lucide';
-import { useEffect } from "react";
-
 type EventDetailsCardProps = {
-  icon: string;
+  faIcon: string;
   title: string;
   children: React.ReactNode;
 }
 
-const EventDetailsCard = ({icon, title, children}: EventDetailsCardProps) => (
-  <>
-    <style jsx>{`
-      svg {
-        width: 56px;
-        height: 56px;
-      }
-  `}</style>
-  <div className="col-lg-3 col-md-6 text-center">
+const EventDetailsCard = ({faIcon, title, children}: EventDetailsCardProps) => (
+    <div className="col-lg-3 col-md-6 text-center">
     <div className="service-box">
-      <i icon-name={icon} className="text-primary sr-icons"></i>
+      <i className={`fa fa-4x ${faIcon} text-primary sr-icons`}></i>
       <h3>{title}</h3>
       <p className="text-muted">
         {children}
       </p>
     </div>
   </div>
-  </>
 )
 
-const EventDetails = () => {
-  useEffect(() => {
-      createIcons({
-          icons: {
-              Send, Mic2, Code, LifeBuoy, HeartHandshake, Baby, Beer, Coins, Gamepad2, Truck, Shirt
-          },
-      });
-  }, [])
-
-  return (
+const EventDetails = () => (
   <section id="event">
     <div className="container">
       <div className="row">
@@ -50,53 +30,53 @@ const EventDetails = () => {
     </div>
     <div className="container">
       <div className="row">
-        <EventDetailsCard icon="send" title="Keynotes">
+        <EventDetailsCard faIcon="fa-paper-plane" title="Keynotes">
             Plusieurs keynotes et tables rondes avec des intervenant⋅e⋅s prestigieux⋅ses sont organisées tout le week-end
         </EventDetailsCard>
 
-        <EventDetailsCard icon="mic-2" title="Conférences">
+        <EventDetailsCard faIcon="fa-bullhorn" title="Conférences">
             Autour de thématiques techniques: C++, développement Web, embarqué, DevOps; de logiciels libres de création graphique et multimédia; de jeux libres; d'enjeux du logiciel libre dans la société et des communautés.<br />
             <a href="https://2019.capitoledulibre.org/programme/#schedule">Retrouvez les conférences de l'édition 2019</a>
         </EventDetailsCard>
 
-        <EventDetailsCard icon="code" title="Ateliers">
+        <EventDetailsCard faIcon="fa-university" title="Ateliers">
             Venez découvrir le logiciel libre lors d'ateliers avec des expert⋅e⋅s pour vous assister.<br /><a href="https://2019.capitoledulibre.org/programme/#schedule">Retrouvez les ateliers de l'édition 2019</a>
         </EventDetailsCard>
 
-        <EventDetailsCard icon="life-buoy" title="Adoptez le logiciel libre">
+        <EventDetailsCard faIcon="fa-life-ring" title="Adoptez le logiciel libre">
             Venez-vous faire aider pour installer Linux, pour corriger les problèmes avec votre Linux ou pour toutes les questions autour du logiciel libre. Un atelier permanent est là pour ça tout le week-end.
         </EventDetailsCard>
       </div>
 
       <div className="row">
-        <EventDetailsCard icon="heart-handshake" title="Village associatif">
+        <EventDetailsCard faIcon="fa-heart" title="Village associatif">
             Retrouvez les associations qui œuvrent pour le logiciel libre: Framasoft, April, Mozilla, Debian, Toulibre,.. <br/>
             <Link href="/village-associatif">Retrouvez la liste des associations présente au village associatif</Link>.
         </EventDetailsCard>
 
-        <EventDetailsCard icon="baby" title="Ateliers pour les enfants">
+        <EventDetailsCard faIcon="fa-child" title="Ateliers pour les enfants">
             Les logiciels libres ludiques pour les plus jeunes à partir de 8 ans de 14h à 18h le samedi et de 14h à 16h le dimanche.
         </EventDetailsCard>
 
-        <EventDetailsCard icon="beer" title="Cocktail">
+        <EventDetailsCard faIcon="fa-glass-martini" title="Cocktail">
             Un moment de convivialité le samedi soir avec intervenant⋅e⋅s, organisateur⋅trice⋅s et public
         </EventDetailsCard>
       </div>
       
       <div className="row">
-        <EventDetailsCard icon="coins" title="Entrée">
+        <EventDetailsCard faIcon="fa-money-check" title="Entrée">
           L'entrée à l'événement est libre et gratuite!
         </EventDetailsCard>
 
-        <EventDetailsCard icon="gamepad-2" title="LAN Party">
+        <EventDetailsCard faIcon="fa-gamepad" title="LAN Party">
           Venez jouer avec vos ami(e)s à des jeux <a href="img/jeux.pdf">libres</a>
         </EventDetailsCard>
 
-        <EventDetailsCard icon="truck" title="Food Truck">
+        <EventDetailsCard faIcon="fa-truck" title="Food Truck">
           Pour vous restaurer, des food truck sont à votre disposition les midis sur le site.
         </EventDetailsCard>
 
-        <EventDetailsCard icon="shirt" title="Boutique de Goodies!">
+        <EventDetailsCard faIcon="fa-shopping-cart" title="Boutique de Goodies!">
           Repartez avec un t-shirt, un hoodie aux couleurs de votre projet libre préféré. Les ventes permettent de financer le Capitole du Libre et de financer les projets libres correspondants.
         </EventDetailsCard>
       </div>
@@ -104,6 +84,5 @@ const EventDetails = () => {
     </div>
   </section>
 )
-}
 
 export default EventDetails;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,13 @@ import Head from 'next/head'
 import Link from 'next/link'
 import React from "react";
 import About from '../components/About';
-import Come from '../components/Come';
 import EventDetails from '../components/EventDetails';
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
 
 export default function Home() {
   return (
-    <>
+    <div className="container">
       <Head>
         <meta charSet="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -405,7 +404,122 @@ export default function Home() {
           </div>
         </section>
 
-        <Come />
+        <section id="come">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <h2 className="section-heading">Comment vous rendre à Toulouse&nbsp;?</h2>
+                <hr className="primary" />
+                <p>Toulouse se trouve à 1h d'avion de Paris, et à 4h de Paris en TGV.</p>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="service-box">
+                  <i className="fa fa-4x fa-car text-primary sr-icons"></i>
+                  <h3>En voiture</h3>
+                  <p className="text-muted">
+                    Si vous souhaitez venir en voiture, nous vous invitons à partager votre trajet
+                    avec d'autres personnes.
+                    Vous pouvez <a href="http://www.openstreetmap.org/directions#map=9/47.9531/-1.8196">calculer votre itinéraire avec Open Street Map</a>
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="service-box">
+                  <i className="fa fa-4x fa-train text-primary sr-icons"></i>
+                  <h3>En train</h3>
+                  <p className="text-muted">Le TGV vous dépose à la gare de Toulouse en plein centre ville, marchez environ 10min le long du Canal du Midi.</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="service-box">
+                  <i className="fa fa-4x fa-bus text-primary sr-icons"></i>
+                  <h3>En bus</h3>
+                  <p className="text-muted">Ouibus, par exemple, propose plusieurs liaisons quotidiennes Paris-Toulouse en 9 heures.</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="service-box">
+                  <i className="fa fa-4x fa-plane text-primary sr-icons"></i>
+                  <h3>En avion</h3>
+                  <p className="text-muted">Il y a un aéroport à Toulouse avec au moins un vol par jour au départ de :
+                    <ul className="list-unstyled">
+                      <li>Lille</li>
+                      <li>Londres</li>
+                      <li>Lyon</li>
+                      <li>Marseille</li>
+                      <li>Nantes</li>
+                      <li>Nice</li>
+                      <li>Paris (Orly & Charles-de-Gaulle)</li>
+                      <li>Rennes</li>
+                      <li>Strasbourg</li>
+                    </ul>
+                  </p>
+                  <p className="text-muted">Plus d'informations disponibles sur le <a href="http://www.toulouse.aeroport.fr/">site de l'aéroport</a>.
+                    L'aéroport est proche de la ville, prenez le tramway jusqu'au Palais de justice où la navette Air France qui vous ramènera à la gare.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-12 text-center">
+                  <h2 className="section-heading">Depuis Toulouse&nbsp;?</h2>
+                  <hr className="primary" />
+                  <p>Le Capitole du Libre sera hébergé à l'<a href="http://www.enseeiht.fr">INP-ENSEEIHT</a>, 2 rue Charles Camichel, 31000 Toulouse.</p>
+                  <p><b>Coordonnées GPS :</b>43.60215 - 1.45524</p>
+                  <iframe id="carte" src="https://umap.openstreetmap.fr/fr/map/cdl_2017_campus_173723?scaleControl=true&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=false&datalayersControl=false&onLoadPanel=undefined&captionBar=false&datalayers=198023#17/43.60215/1.45524"></iframe>
+                  <p><a href="http://umap.openstreetmap.fr/en/map/cdl_2017_campus_173723#17/43.60215/1.45524">Voir en plein écran</a></p>
+                </div>
+              </div>
+            </div>
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-3 col-md-6 text-center">
+                  <div className="service-box">
+                    <i className="fa fa-4x fa-male text-primary sr-icons"></i>
+                    <h3>À pied</h3>
+                    <p className="text-muted">L'école est idéalement située en centre ville. Si vous êtes en ville, vous pouvez rejoindre l'école située sur les bords du canal à pied.
+                      Vous êtes situé⋅e⋅s à moins de 10min à pied de la fameuse place du Capitole.</p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-6 text-center">
+                  <div className="service-box">
+                    <i className="fa fa-4x fa-subway text-primary sr-icons"></i>
+                    <h3>En Métro</h3>
+                    <p className="text-muted">L'école est située à 5min à pied de l'arrêt François Verdier sur la ligne de métro B.
+                      Les métros circulent jusqu'à 0h00 du dimanche au jeudi et jusqu'à 3h les vendredis et samedis.
+                      Pour plus d'informations sur les horaires des bus et métros vous pouvez
+                      consulter <a href="http://www.tisseo.fr/se-deplacer/plans">le site du Réseau Métro/Bus/Tramway de Toulouse</a>
+                    </p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-6 text-center">
+                  <div className="service-box">
+                    <i className="fa fa-4x fa-bus text-primary sr-icons"></i>
+                    <h3>En Bus</h3>
+                    <p className="text-muted">L'école est située à proximité de nombreuses lignes de bus: 14, 22, 23, 27, 38
+                      Les bus et tramway circulent jusqu'à 0h30 du dimanche au jeudi et jusqu'à 1h00 les vendredis et samedi.
+                      Pour plus d'informations sur les horaires des bus et métros vous pouvez
+                      consulter <a href="http://www.tisseo.fr/se-deplacer/plans">le site du Réseau Métro/Bus/Tramway de Toulouse</a>
+                    </p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-6 text-center">
+                  <div className="service-box">
+                    <i className="fa fa-4x fa-bicycle text-primary sr-icons"></i>
+                    <h3>En vélo libre service</h3>
+                    <p className="text-muted">Toulouse dispose d'un système de vélos en libre service complémentaire des transports en commun : <a href="http://www.velo.toulouse.fr/">Vélô Toulouse</a>.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
         <section id="accomodation">
           <div className="container">
@@ -544,6 +658,6 @@ export default function Home() {
         <script src="/js/creative.min.js"></script>
 
       </main>
-    </>
+    </div>
   )
 }
