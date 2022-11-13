0feb793c4766989b7dbb0db48817dc6c366d848d
Lionel Porcheron <lionel.porcheron@bleemeo.com>
2022-11-13 18:11:14 +0100

    Add blog page

---
+++ pages/blog/programme-cdl-2022.tsx
@@ -0,0 +1,71 @@
+import NavBar from "../../components/NavBar";
+import Head from "next/head";
+
+const ProgrammeCDL2022 = () => {
+    return <>
+        <Head>
+            <script type="text/javascript" src="https://cfp.capitoledulibre.org/cdl-2022/schedule/widget/v2.en.js"></script>
+        </Head>
+        <NavBar />
+
+           <section id="blog">
+                <div className="container">
+                    <div className="row">
+
+<h1>Élaboration du programme du Capitole du Libre 2022</h1>
+
+<p>Ces derniers jours, de nombreuses réactions sur les réseaux sociaux nous conduisent à expliquer comment le programme de cette édition a été élaboré.</p>
+
+<p>Le <b>Capitole du Libre est un "Week-end dédié au logiciel libre"</b>, notre premier critère d'évaluation est que la proposition soit en rapport avec le Logiciel Libre ou le Libre de manière générale. Ce critère est en accord avec les status de l'association qui organise le Capitole du Libre, à savoir l'association Toulibre.</p>
+
+<p>Lors des différentes éditions du Capitole du Libre, nous avons toujours tenu à donner leur chance à des speakers débutants à l'inverse de nombreuses conférences où la sélection est plus forte. Il en a été de même lors de cette édition.</p>
+
+<p>Pour l’élaboration du programme, un groupe de 6 personnes a voté pour chaque proposition. Ce groupe est indépendant du staff, même si plusieurs membres du Staff participent également aux votes. Seul le texte de la proposition est un critère de vote. Les notes vont de 0 à 2 (0: pas pour moi, 1: neutre, 2: oui pour moi). Les propositions avec une notre médiane de moins de 1 ont toutes été refusées cette année.</p>
+
+<p>Quelques statistiques sur les propositions que nous avons reçues cette année:</p>
+<ul>
+<li>67 propositions de conférence de 50min</li>
+<li>54 propositions de conférence de 25min</li>
+<li>22 propositions d'atelier de 2h</li>
+<li>39 propositions de stands associatifs</li>
+</ul>
+
+<p>Ces 182 propositions émanent de 133 speakers:</p>
+<ul>
+<li>99 hommes</li>
+<li>15 femmes</li>
+<li>5 non connu</li>
+<li>14 associations</li>
+</ul>
+
+<p>Sur ces 182 propositions, 156 ont été retenues, dont 38 stands, soit un total de 118 conférences ou ateliers.</p>
+
+<p>Les propositions retenues émanent de 119 conférencier.e.s :</p>
+<ul>
+<li>92 hommes</li>
+<li>10 femmes</li>
+<li>4 non connu</li>
+<li>13 associations</li>
+</ul>
+
+<p>Nous comprenons et reconnaissons volontiers que la mixité et la diversité atteinte sur le panel des speakers n'est pas celle que nous voudrions et ne correspond pas aux ambitions que nous nous étions fixées. L'objectif de ce Capitole du Libre 2022 était de relancer la dynamique "d'avant la pandémie". Certaines choses n'ont pas abouties faute de temps de la part de nos équipes de bénévoles.</p>
+
+<p>Si une édition 2023 devait avoir lieu, nous anticiperons mieux l'appel à participation pour nous laisser plus de temps sur la sélection des propositions et la construction du programme.
+Nous avons également besoin de l'aide de la communauté du Libre pour nous aider à nous améliorer sur ces points dans le futur.</p>
+
+<p>Une table ronde sera organisée pour aborder ce sujet le Dimanche à 10h00 pendant le Capitole du Libre.</p>
+
+<p><b>Nous sommes désolés que certaines personnes aient pu se sentir rejetées de part leur genre, religion, couleur ou tout autre critère qui serait discriminatoire. Notre volonté est de faire du Capitole du Libre un événement bienveillant ou chacun.e se retrouve pour échanger autour du Libre et du Logiciel Libre. Nos efforts ont été insuffisants, nous en prenons bonne note pour le jour J et les éditions futures.</b></p>
+
+<p>L'équipe d'organisation du Capitole du Libre</p>
+
+
+
+                    </div>
+                </div>
+            </section>
+        </>
+
+}
+
+export default ProgrammeCDL2022
