7648c7723c0ae056009caeb8ce1605f9cdcb9f00
Lionel Porcheron <lionel.porcheron@bleemeo.com>
2024-10-16 23:07:21 +0200

    Add 2024 sponsors on website

---
+++ components/Partners.tsx
@@ -0,0 +1,135 @@
+const Partners = () => (
+<div>
+    <div className="row text-center cdl-partner-category">
+    <div className="col-xs-12 col-sm-12">
+      <h3>Platine</h3>
+      <a href="http://bootlin.com" target="_blank">
+          <img src="/img/bootlin-logo-240.png" alt="Bootlin" className="logo-200px" title="Bootlin" />
+      </a>
+      &nbsp;
+      &nbsp;            
+      <a href="https://www.centreon.com" target="_blank">
+        <img src="/img/logo-centreon.png" alt="Centreon" className="logo-200px" title="Centreon" />
+      </a>      
+      &nbsp;     
+      &nbsp;                                            
+      <a href="https://www.csgroup.eu/fr" target="_blank">
+        <img src="/img/logo-cs-group.svg" alt="CS" className="logo-150px" title="CS" />
+      </a>
+      &nbsp;
+      &nbsp;                                                            
+      <a href="https://www.thalesgroup.com" target="_blank">
+        <img src="/img/logo-thales.svg" alt="Thales Group" className="logo-200px" title="Thales Group" />
+      </a>                
+    </div>
+  </div>
+
+  <div className="row text-center cdl-partner-category">
+    <div className="col-xs-12 col-sm-12">
+      <h3>Or</h3>       
+      <a href="https://www.linagora.com" target="_blank">
+        <img src="/img/logo-linagora.png" alt="Linagora" className="logo-150px" title="Linagora" />
+      </a>
+      &nbsp;
+      &nbsp;
+      <a href="https://www.logilab.fr" target="_blank">
+        <img src="/img/logo-logilab.svg" alt="Logilab" className="logo-150px" title="Linagora" />
+      </a>
+      &nbsp;
+      &nbsp;                                     
+      <a href="https://makina-corpus.com/" target="_blank">
+        <img src="/img/makina-corpus.png" alt="Makina Corpus" className="logo-100px" title="Makina Corpus" />
+      </a>
+      &nbsp;
+      &nbsp;                 
+      <a href="https://www.viveris.fr" target="_blank">
+        <img src="/img/logo-viveris.svg" alt="Viveris" className="logo-150px" title="Viveris" />
+      </a>
+    </div>
+  </div>
+
+  <div className="row text-center cdl-partner-category">
+    <div className="col-xs-12 col-sm-12">
+      <h3>Argent</h3>
+      <a href="https://www.data-bene.io" target="_blank">
+        <img src="/img/logo-databene.png" alt="DataBene" className="logo-100px" title="DataBene" />
+      </a>
+      &nbsp;
+      <a href="https://evolix.com" target="_blank">
+        <img src="/img/logo-evolix.png" alt="Evolix" className="logo-100px" title="Evolix" />
+      </a>
+      &nbsp;        
+      <a href="https://www.smile.eu" target="_blank">
+        <img src="/img/SMILE_icone-full-orange -baseline.png" alt="Smile" className="logo-100px" title="Smile" />
+      </a>
+      &nbsp;                
+      <a href="https://www.worteks.com" target="_blank">
+        <img src="/img/logo-worteks.png" alt="Worteks" className="logo-100px" title="Worteks" />
+      </a>
+      &nbsp;                
+    </div>
+  </div>
+
+  <div className="row text-center cdl-partner-category">
+    <div className="col-xs-12 col-sm-12">
+      <h3>Bronze</h3>
+      <p>
+        <a href="https://bleemeo.com" target="_blank">
+          <img src="/img/bleemeo_logo.svg" alt="Bleemeo" className="logo-100px" title="Bleemeo" />
+        </a>
+        &nbsp;
+        <a href="https://www.bluemind.net/" target="_blank">
+          <img src="/img/logo-bluemind.png" alt="Bluemind" className="logo-100px" title="Bluemind" />
+        </a>
+        &nbsp;
+        <a href="https://www.loxodata.frr" target="_blank">
+        <img src="/img/logo-loxodata.png" alt="Loxodata" className="logo-100px" title="Linagora" />
+      </a>
+      &nbsp;        
+      </p>
+    </div>
+  </div>
+
+  <div className="row text-center">
+    <div className="col-lg-4">
+      <h3>Organisation</h3>
+      <a href="http://toulibre.org/" target="_blank">
+        <img src="/img/logo-toulibre.svg" alt="Toulibre" className="logo-150px" title="Toulibre" />
+      </a>
+    </div>
+    <div className="col-lg-4">
+      <h3>Support Institutionnels</h3>
+      <a href="http://enseeiht.fr/" target="_blank">
+        <img src="/img/logo-toulouse-inp-n7.png" alt="INP-N7" className="logo-150px" title="INP-N7" />
+      </a>
+      &nbsp;
+      <a href="http://www.toulouse.fr" target="_blank">
+        <img src="/img/Logo_mairie_de_Toulouse.svg" alt="Toulouse" className="logo-150px" title="Mairie de Toulouse" />
+      </a>
+      &nbsp;
+      <a href="https://www.laregion.fr" target="_blank">
+        <img src="/img/1200px-Logo_Occitanie_2017.svg.png" alt="La Region Occitanie" className="logo-100px" title="La Region Occitanie" />
+      </a>
+      &nbsp;
+      <a href="https://www.haute-garonne.fr/" target="_blank">
+        <img src="/img/logo-hg31.svg" alt="Conseil Départemetal de la Haute Garonne" className="logo-100px" title="Conseil Départemetal de la Haute Garonne" />
+      </a>                
+    </div>
+    <div className="col-lg-4">
+      <h3>Communication</h3>
+      <div className="row">
+        <a href="http://www.editions-diamond.fr/" target="_blank">
+          <img src="/img/logo-edition-diamonds.png" alt="Éditions Diamonds" className="logo-100px" title="Éditions Diamonds" />
+        </a>
+        &nbsp;
+        <a href="https://tisseo-collectivites.fr/" target="_blank">
+          <img src="/img/logo-tisseo-collectivites.png" alt="Tisséo Collectivités" className="logo-100px" title="Tisséo Collectivités" />
+        </a>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  );
+  
+  export default Partners;
