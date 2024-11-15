76f87e9004e70790727a845347cc8c0cbf65d6db
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-11-15 14:04:04 +0100

    Add license to crepes recipe

--- pages/stand-crepes/index.tsx
+++ pages/stand-crepes/index.tsx
@@ -1,5 +1,4 @@
 import NavBar from '../../components/NavBar';
-import Partners from '../../components/Partners'
 import styles from './index.module.css'
 
 export default function StandCrepes() {
@@ -8,7 +7,14 @@ export default function StandCrepes() {
 			<NavBar />
 			<section className={styles.summary}>
 				<div className={styles.container}>
-					<h1 className={styles.title}>Les crêpes</h1>
+					<h1 className={styles.title}>
+						Les crêpes
+
+						<a href="#license" className={styles.license}><img
+							src="http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-4.png"
+							width="80" height="15" alt="WTFPL" />
+						</a>
+					</h1>
 					<img src="/img/crepes.png" alt="Crêpes" className={styles.result} title="Les crêpes du Capitole du Libre" />
 				</div>
 				<div className={styles.recipe}>
@@ -27,6 +33,13 @@ export default function StandCrepes() {
 					Nous commencerons par battre les œufs, puis nous ajouterons le sucre, le sucre vanillé, le sel. Une fois bien mélangé, nous allons ajouter au fur et à mesure la farine, lorsque mélanger devient trop difficile ajouter un petit peu de lait. Nous finirons par ajouter le reste de lait et le beurre et nous mélangerons bien. Laisser posez puis faite cuire sur une billig à 230°C.
 				</div>
 			</section>
+
+			<p className={styles.license_txt} id="license">
+				Copyright © 2024 Capitole du Libre {"<contact@capitoledulibre.org>"}<br />
+				This work is free. You can redistribute it and/or modify it under the
+				terms of the Do What The Fuck You Want To Public License, Version 2,
+				as published by Sam Hocevar. See <a href="http://www.wtfpl.net/">www.wtfpl.net</a> for more details.
+			</p>
 		</>
 	)
 }
